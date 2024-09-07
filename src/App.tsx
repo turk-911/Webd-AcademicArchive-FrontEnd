475410020d08be5982e4fe2573ed8f125faaa776
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-09-07 15:57:40 +0530

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import SpaceCanvas from './components/Canvas/SpaceCanvas'
-import Header from './components/Header/Header'
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import SignupPage from "./components/Authentication/SignupPage";
+import LoginPage from "./components/Authentication/LoginPage";
+import "./App.css";
+import HeroPage from "./components/Hero/HeroPage";
 const App: React.FC = () => {
   return (
-    <>
-      <SpaceCanvas />
-      <Header /> 
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
   );
-}
-export default App 
+};
+
+export default App;
